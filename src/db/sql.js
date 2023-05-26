58eb1949702c6f99a348b2c6512508a3cedc3ddb
Corro-Kun <networpava@gmail.com>
2023-05-25 23:39:23 -0500

    fixed bugs

--- src/db/sql.js
+++ src/db/sql.js
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise";
+import { PORT_DB, HOST_DB, USER_DB, PASSWORD_DB, DATABASE_DB } from "../config.js";
 
 const sql = createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "pava",
-    database: "library"
+    host: HOST_DB,
+    port: PORT_DB,
+    user: USER_DB,
+    password: PASSWORD_DB,
+    database: DATABASE_DB
 })
 
 export default sql
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import { app } from "./app.js";
 import morgan from "morgan"
 import express from "express";
+import { PORT } from "./config.js";
+import cors from "cors"
 
 const App = express();
+
+App.use(cors());
 
 App.use(app);
 
 App.use(morgan("dev"));
 
-App.listen(3000);
+App.listen(PORT);
 
-console.log('port open on 3000');
+console.log('port open on', PORT);
